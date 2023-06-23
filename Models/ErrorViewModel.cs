f17c1662ee0133ad652b834f576f6d3ee955744d
Anthony Rodriguez <anthonyr38@proton.me>
2023-06-22 20:46:37 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace asp_net_core_MVC_1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
